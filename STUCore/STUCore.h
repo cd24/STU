e3a987bdf8f10ec2a9b9d5a15693ff3207447d6c
John McAvey <jmc94@icloud.com>
2020-04-25 15:57:23 -0700

    STU-2
    Added STUCore target, base configuration

---
+++ STUCore/STUCore.h
@@ -0,0 +1,19 @@
+//
+//  STUCore.h
+//  STUCore
+//
+//  Created by John McAvey on 4/25/20.
+//  Copyright © 2020 John McAvey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STUCore.
+FOUNDATION_EXPORT double STUCoreVersionNumber;
+
+//! Project version string for STUCore.
+FOUNDATION_EXPORT const unsigned char STUCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STUCore/PublicHeader.h>
+
+
